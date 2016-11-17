fac5b54f77c2d8fc339faa103c4ad7e5c3ad09a5
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-11-17 22:15:48 +0100

    adds quiet flag to git clone command

--- bin/preinstall.sh
+++ bin/preinstall.sh
@@ -3,5 +3,5 @@
 git config -f .gitmodules --get-regex submodule\.\*url \
   | sed 's/submodule.//g;s/.url//g' \
   | awk '{print $2,$1}' \
-  | xargs -n2 git clone --recursive \
+  | xargs -n2 git clone --quiet --recursive \
   | sh
